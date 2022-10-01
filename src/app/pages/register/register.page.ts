2d352b69e437991656051638ccec9f0672e5049d
Vinícius Martins <viniciusmartinsg@hotmail.com>
2022-09-30 22:25:47 -0300

    feat(register): added validation into register page

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CreateUserPayload, RegisterPayload } from '../../models/payloads/register.payload';
+import { CustomValidators } from '../../utils/validators';
+import isValidEmail = CustomValidators.isValidEmail;
+import isValidPassword = CustomValidators.isValidPassword;
 
 @Component({
   selector: 'app-register',
@@ -16,12 +19,30 @@ export class RegisterPage implements OnInit {
   ngOnInit() {
   }
 
-  public user: RegisterPayload = {
+  public registerPayload: RegisterPayload = {
     name: '',
     email: '',
     city: '',
     password: '',
     confirmPassword: '',
   };
+
+  public isLoading: boolean = false;
+
+  public async register(): Promise<void> {
+    if(!this.canRegister()) return;
+    this.isLoading = true;
+
+    await this.router.navigate(['/home']);
+
+    this.isLoading = false;
+  }
+
+  public canRegister(): boolean {
+    if (isValidEmail(this.registerPayload.email) && isValidPassword(this.registerPayload.password) && isValidPassword(this.registerPayload.confirmPassword) && this.registerPayload.name.length > 6 && this.registerPayload.city.length > 3)
+      return true;
+
+    return false;
+  }
 
 }
