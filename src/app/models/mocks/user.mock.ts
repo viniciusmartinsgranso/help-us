71ff18318d60a5b0d3e20d7f93ca6a7224002b06
Vinícius Martins <viniciusmartinsg@hotmail.com>
2022-12-17 23:44:00 -0300

    -

--- src/app/models/mocks/user.mock.ts
+++ src/app/models/mocks/user.mock.ts
@@ -5,21 +5,21 @@ export const mockedUsers: UserProxy[] = [
     city: 'Sorocaba',
     name: 'Vinicius',
     email: 'vinicius@email.com',
-    id: 0,
+    id: 10000000000,
     photoUrl: 'assets/images/vini.jpg'
   },
   {
     city: 'Sorocaba',
     name: 'Eduardo',
     email: 'eduardo@email.com',
-    id: 0,
+    id: 10000000001,
     photoUrl: 'assets/images/Eduardo.jpg'
   },
   {
     city: 'Sorocaba',
     name: 'Lucas',
     email: 'lucas@email.com',
-    id: 0,
+    id: 10000000002,
     photoUrl: 'assets/images/Lucas.jpg'
   }
 ];
